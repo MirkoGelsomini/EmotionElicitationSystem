1b1c35eb5ca67615e7a35a330102de0b921be2ba
RRasty00 <78664601+RRasty00@users.noreply.github.com>
2022-07-28 17:48:49 +0200

    added some comments

--- Application/app-assets/js/applicationJs/question.js
+++ Application/app-assets/js/applicationJs/question.js
@@ -147,7 +147,9 @@ saveBtn.onclick = function () {
     save();
     thanksModal.hide();
 }
-
+/**
+ * Function which check if all question have been answerd
+ */
 function confirm(){
     let questionsAnswer = [];
 
@@ -258,6 +260,10 @@ function createRandomizedQuestion() {
 
 //--End Randomize Question--
 
+/**
+ * function that generate an unique uuid id for the session
+ * @returns sessionId
+ */
 function uuidv4() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
         var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
